7d5bcc0ba3a59d16ba62383905d3bcc37f14de61
Виктория Тонявина <viktoriatonavina@MacBook-Air-Viktoria.local>
2022-10-17 22:37:47 +0300

    add task 2

---
+++ HW/HW2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 2 (Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел).
+
+Console.Write("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+int max = n1;
+if (n2 > max) 
+{
+    max = n2;
+}
+if (n3 > max)
+{
+    max = n3;
+}
+
+Console.WriteLine("Максимальное число: " + max);
