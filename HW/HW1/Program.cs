979662a8d627c208cc8302a61161f5b4636f1a54
Виктория Тонявина <viktoriatonavina@MacBook-Air-Viktoria.local>
2022-10-17 22:34:27 +0300

    add task 1

---
+++ HW/HW1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 1 (Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее).
+
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+if (n > n1)
+{
+    Console.WriteLine("Больше число: " + n);
+    Console.WriteLine("Меньшее число: " + n1);
+}
+else
+{
+    Console.WriteLine("Большее число: " + n1);
+    Console.WriteLine("Меньшее число: " + n);
+}
