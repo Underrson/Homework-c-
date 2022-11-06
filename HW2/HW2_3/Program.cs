ad8f1b677eefa6ba850a2d62247c981359c96f12
Виктория Тонявина <viktoriatonavina@MacBook-Air-Viktoria.local>
2022-11-07 00:31:42 +0300

    add task 3

---
+++ HW2/HW2_3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("данная цифра не может быть днем недели");
+  }
+  else Console.WriteLine("(этот день не выходной) -> нет");
+
+
