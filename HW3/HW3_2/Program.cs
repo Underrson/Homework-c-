3a469059b99c90870fa8e0a444fb6665af361eb1
Виктория Тонявина <viktoriatonavina@MacBook-Air-Viktoria.local>
2022-11-10 19:44:28 +0300

    add task 2

---
+++ HW3/HW3_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Write("Введите X1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите X2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 -y2, 2) + Math.Pow(z1 - z2, 2));
+
+Console.WriteLine($"d = {d:f3}");
