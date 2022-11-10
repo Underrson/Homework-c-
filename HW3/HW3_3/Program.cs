b4fc3f2e9f4469ada0dfce0237ac086080641b8c
Виктория Тонявина <viktoriatonavina@MacBook-Air-Viktoria.local>
2022-11-10 20:28:35 +0300

    add task 3

---
+++ HW3/HW3_3/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число:  ");
+int N = Convert.ToInt32 (Console.ReadLine());
+int i = 1;
+while (i < N)
+{
+    i++;
+    Console.WriteLine ($"{i * i * i}");
+}
