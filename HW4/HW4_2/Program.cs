05536ecc327d7d86ab24d8c6cdb39e9a81f0ae2a
Виктория Тонявина <viktoriatonavina@MacBook-Air-Viktoria.local>
2022-11-14 17:14:27 +0300

    add task 2

---
+++ HW4/HW4_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Cумм цифр = {GetSumNums(numberN)}");
+
+  int GetSumNums (int num)
+  {
+    
+    int sum = 0;
+    while (num>0)
+    {
+      sum += num % 10;
+      num /= 10;
+    }
+   return sum;
+  }
+
+
