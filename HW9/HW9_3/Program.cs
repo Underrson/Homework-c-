2be0ccb49e0786061176f698cc74628f1e0b8547
Виктория Тонявина <viktoriatonavina@MacBook-Air-Viktoria.local>
2022-12-12 18:19:01 +0300

    add task 3

---
+++ HW9/HW9_3/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.Clear();
+
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"A({n}, {m}) = {Akkerman(n, m)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
