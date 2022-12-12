0089639f401e1431456228097762dc25b18b1a9f
Виктория Тонявина <viktoriatonavina@MacBook-Air-Viktoria.local>
2022-12-12 18:14:27 +0300

    add task 2

---
+++ HW9/HW9_2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма элементов от {n} до {m} = {CountNaturalSum(n, m)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalSum(int n, int m)
+{
+    if (n == m)
+        return m;
+    return m + CountNaturalSum(n, m - 1);
+}
