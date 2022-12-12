2a6a95b96093a8bd196ea0766ad379f78731a341
Виктория Тонявина <viktoriatonavina@MacBook-Air-Viktoria.local>
2022-12-12 18:02:09 +0300

    add task 1

---
+++ HW9/HW9_1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Clear();
+
+Console.WriteLine("Введите положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
